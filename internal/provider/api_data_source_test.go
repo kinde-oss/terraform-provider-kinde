53df3155fe0c1c676fb5d71fb1ea43b57c5cd941
Koosha Owji <koosha.owji@gmail.com>
2026-08-18 13:41:03 +1000

    test: make acceptance tests safe to run concurrently

--- internal/provider/api_data_source_test.go
+++ internal/provider/api_data_source_test.go
@@ -4,36 +4,41 @@
 package provider
 
 import (
+	"fmt"
 	"testing"
 
+	"github.com/hashicorp/terraform-plugin-testing/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 )
 
 func TestAccAPIDataSource(t *testing.T) {
+	// Audiences must be unique within a Kinde business.
+	testID := acctest.RandomWithPrefix("tfacc-")
+
 	resource.Test(t, resource.TestCase{
 		PreCheck:                 func() { testAccPreCheck(t) },
 		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccAPIDataSourceConfig(),
+				Config: testAccAPIDataSourceConfig(testID),
 				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr("data.kinde_api.test", "name", "Terraform Acceptance Test API"),
-					resource.TestCheckResourceAttr("data.kinde_api.test", "audience", "https://registry.terraform.io/providers/kinde-oss/kinde"),
+					resource.TestCheckResourceAttr("data.kinde_api.test", "name", testID),
+					resource.TestCheckResourceAttr("data.kinde_api.test", "audience", testID),
 				),
 			},
 		},
 	})
 }
 
-func testAccAPIDataSourceConfig() string {
-	return `
+func testAccAPIDataSourceConfig(testID string) string {
+	return fmt.Sprintf(`
 resource "kinde_api" "test" {
-	name     = "Terraform Acceptance Test API"
-	audience = "https://registry.terraform.io/providers/kinde-oss/kinde"
+	name     = %[1]q
+	audience = %[1]q
 }
 
 data "kinde_api" "test" {
 	id = kinde_api.test.id
 }
-`
+`, testID)
 }
